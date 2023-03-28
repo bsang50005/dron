1806b0772bab17f76652546d7e137cdf15dc1b64
bsang50005 <62686419+bsang50005@users.noreply.github.com>
2023-03-28 12:48:27 +0900

    Update 3-88888.py

--- 3-88888.py
+++ 3-88888.py
@@ -1,50 +1,18 @@
-import time
 import cv2 as cv
-import matplotlib.pyplot as plt
-import numpy as np
 
-img=cv.imread('air.jpg')
+img=cv.imread('ddc.png')
+patch=img[250:350,170:270,:]
 
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-plt.imshow(gray,cmap='gray'),plt.xticks([]),plt.yticks([]),plt.show()
 
-cv.imwrite('gray.jpg',gray)
+img=cv.rectangle(img,(170,250),(270,350),(255,0,0),3)
+patch1=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_NEAREST)
+patch2=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_LINEAR)
+patch3=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_CUBIC)
 
-h=cv.calcHist([gray],[0],None,[256],[0,256])
-plt.plot(h,color='r',linewidth=1),plt.show()
+cv.imshow('Original',img)
+cv.imshow('Resize nearest',patch1) 
+cv.imshow('Resize bilinear',patch2) 
+cv.imshow('Resize bicubic',patch3) 
 
-equal=cv.equalizeHist(gray)
-plt.imshow(equal,cmap='gray'),plt.xticks([]),plt.yticks([]),plt.show()
-
-cv.imwrite('equals.jpg',equal)
-
-h=cv.calcHist([equal],[0],None,[256],[0,256])
-plt.plot(h,color='r',linewidth=1),plt.show()
-
-def my_cvtGray1(bgr_img):
-    g=np.zeros([bgr_img.shape[0],bgr_img.shape[1]])
-    for r in range(bgr_img.shape[0]):
-        for c in range(bgr_img.shape[1]):
-            g[r,c]=0.144*bgr_img[r,c,0]+0.587*bgr_img[r,c,1]+0.299*bgr_img[r,c,2]
-    return np.uint8(g)
-
-def my_cvtGray2(bgr_img):
-    g=np.zeros([bgr_img.shape[0],bgr_img.shape[1]])
-    g=0.144*bgr_img[:,:,0]+0.587*bgr_img[:,:,1]+0.299*bgr_img[:,:,2]
-    return np.uint8(g)
-
-img=cv.imread('soccer.jpg')
-img1=cv.imread('gray.jpg')
-img2=cv.imread('equals.jpg')
-
-start=time.time()
-my_cvtGray1(img1)
-print('My time1:',time.time()-start)
-
-start=time.time()
-my_cvtGray2(img2)
-print('My time2:', time.time()-start)
-
-start=time.time()
-cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-print('OpenCV time:',time.time()-start)
+cv.waitKey()
+cv.destroyAllWindows()
